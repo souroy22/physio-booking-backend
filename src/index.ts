cc77552f6ecf938cc1c0eda11a5cbad59fff4a19
Sourav Roy <60303447+souroy22@users.noreply.github.com>
2024-11-08 12:27:38 +0530

    added cors configuration

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import cors from "cors";
+import { corsOptions } from "./configs/corsConfig";
 
 // files
 import router from "./routers";
@@ -24,7 +25,7 @@ declare global {
 }
 
 app.use(express.json({ limit: "10kb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.get("/", (req: Request, res: Response) => {
